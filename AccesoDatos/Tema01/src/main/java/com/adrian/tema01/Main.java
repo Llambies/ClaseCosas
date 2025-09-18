41adcad4d639cbd87d00d1604e96d21092c8fe05
Owoma <adrmallla17@gmail.com>
2025-09-18 15:48:35 +0200

    Hashmap

--- AccesoDatos/Tema01/src/main/java/com/adrian/tema01/Main.java
+++ AccesoDatos/Tema01/src/main/java/com/adrian/tema01/Main.java
@@ -1,9 +1,26 @@
 package com.adrian.tema01;
 
 import com.adrian.tema01.boletin01.*;
+import com.adrian.tema01.mapeoManual.Mapeador;
 
 public class Main {
+
+    static String path = "./src/main/java/com/adrian/tema01/boletin01/";
+
     public static void main(String[] args) {
+        Mapeador<String,Integer> mapeador = new Mapeador();
+
+        mapeador.agregar("hhh",1);
+        mapeador.agregar("b",2);
+        mapeador.agregar("hhg",2);
+
+        System.out.println(mapeador.buscar("hhg"));
+        System.out.println(mapeador.buscar("hdg"));
+
+
+    }
+
+    void ejecutarEjercicios(String[] args) {
         new Ej01().main(args);
         new Ej02().main(args);
         new Ej03().main(args);
@@ -12,11 +29,11 @@ public class Main {
         new Ej06().main(args);
         new Ej07().main(args);
         String[] argsEj08 = new String[1];
-        argsEj08[0] = "./src/main/java/com/adrian/tema01/boletin01/Ej04.java";
+        argsEj08[0] = path + "Ej04.java";
         new Ej08().main(argsEj08);
         String[] argsEj09 = new String[2];
-        argsEj09[0] = "./src/main/java/com/adrian/tema01/boletin01/Ej04.java";
-        argsEj09[1] = "./src/main/java/com/adrian/tema01/boletin01/Ej05.java";
+        argsEj09[0] = path + "Ej04.java";
+        argsEj09[1] = path + "Ej05.java";
         new Ej09().main(argsEj09);
         new Ej10().main(argsEj09);
         new Ej11().main(argsEj09);
---
+++ AccesoDatos/Tema01/src/main/java/com/adrian/tema01/mapeoManual/Mapeador.java
@@ -0,0 +1,64 @@
+package com.adrian.tema01.mapeoManual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mapeador<K, V> {
+
+    Object[] baser = new Object[256];
+
+
+    public ArrayList<Integer> calcularIndices(K key) {
+        int hash = key.hashCode();
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        while (hash > 256) {
+            indices.add(hash % 256);
+            hash = hash / 256;
+        }
+        indices.add(hash);
+        indices.add(0);
+        return indices;
+    }
+
+    public void agregar(K key, V value) {
+        ArrayList<Integer> indices = calcularIndices(key);
+        System.out.println(indices);
+        insertar(indices, 0, value, baser);
+    }
+
+    void insertar(ArrayList<Integer> key, int keyActual, V value, Object[] lista) {
+        if (keyActual == key.size() - 1) {
+            // Condición base: almacena el valor
+            lista[key.get(keyActual)] = value;
+            return;
+        }
+
+        if (lista[key.get(keyActual)] == null) {
+            lista[key.get(keyActual)] = new Object[256];
+        }
+
+        insertar(key, keyActual + 1, value, (V[]) lista[key.get(keyActual)]);
+    }
+
+    public V buscar(K key) {
+        ArrayList<Integer> indices = calcularIndices(key);
+        System.out.println(indices);
+        return obtener(indices, 0, baser);
+
+    }
+
+    V obtener(ArrayList<Integer> key, int keyActual, Object[] lista) {
+        System.out.println("keyActual: " + keyActual + " key: " + key.get(keyActual));
+        System.out.println(lista);
+        if (lista[key.get(keyActual)] == null) {
+            return null;
+        }
+        if (keyActual == key.size() - 1) {
+            return (V) lista[key.get(keyActual)];
+        } else {
+            return obtener(key, keyActual + 1, (Object[]) lista[key.get(keyActual)]);
+        }
+
+    }
+
+}
